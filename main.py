86173b87a519028df8ab7c45fe1b4cc75bb5515f
Radomyr <qulowg@gmail.com>
2024-07-21 11:07:38 +0300

    v 0.2 - added sending to the server and frontend

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Form
 from pydantic import BaseModel
 from temscale import temscale
+from typing import Annotated
 
 app = FastAPI()
 
 
 class TemscaleModel(BaseModel):
     value: int
-    type: str
+    old_type: str
+    new_type: str
+
+
+@app.post("/convert_json")
+async def convert(temscale_model: TemscaleModel):
+    try:
+        tem = temscale.Temscale(temscale_model.value, temscale_model.old_type)
+        tem.convert(temscale_model.new_type)
+        return temscale.to_dict(tem)
+    except TypeError:
+        return HTTPException(status_code=400, detail="Invalid type")
 
 
 @app.post("/convert")
-async def convert(temscale_model: TemscaleModel, new_type: str):
+async def convert(value: Annotated[int, Form()],
+                  old_type: Annotated[str, Form()],
+                  new_type: Annotated[str, Form()]):
     try:
-        tem = temscale.Temscale(temscale_model.value, temscale_model.type)
+        tem = temscale.Temscale(value, old_type)
         tem.convert(new_type)
         return temscale.to_dict(tem)
     except TypeError:
