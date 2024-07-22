9796304a2bea6a1a5ff58621e82274a6d9b62f63
Radomyr <qulowg@gmail.com>
2024-07-23 01:29:29 +0300

    v 0.3 - redesigned sending a request - added request trimming

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from fastapi import FastAPI, HTTPException, Form
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from temscale import temscale
-from typing import Annotated
 
 app = FastAPI()
+
+
+origins = [
+    "*",
+    "http://127.0.0.1:8000/",
+    "http://127.0.0.1/",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class TemscaleModel(BaseModel):
@@ -12,23 +28,11 @@ class TemscaleModel(BaseModel):
     new_type: str
 
 
-@app.post("/convert_json")
+@app.post("/convert")
 async def convert(temscale_model: TemscaleModel):
     try:
         tem = temscale.Temscale(temscale_model.value, temscale_model.old_type)
         tem.convert(temscale_model.new_type)
-        return temscale.to_dict(tem)
-    except TypeError:
-        return HTTPException(status_code=400, detail="Invalid type")
-
-
-@app.post("/convert")
-async def convert(value: Annotated[int, Form()],
-                  old_type: Annotated[str, Form()],
-                  new_type: Annotated[str, Form()]):
-    try:
-        tem = temscale.Temscale(value, old_type)
-        tem.convert(new_type)
         return temscale.to_dict(tem)
     except TypeError:
         return HTTPException(status_code=400, detail="Invalid type")
